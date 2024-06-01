2251348fd49d498c6d805019abc2dacd7f427b4b
Samuel Hoyas <samuelix97@gmail.com>
2024-06-01 14:04:32 +0200

    Electron + vite

---
+++ vite.main.config.ts
@@ -0,0 +1,36 @@
+import autoprefixer from 'autoprefixer';
+import type { ConfigEnv, UserConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
+import { getBuildConfig, getBuildDefine, external, pluginHotRestart } from './vite.base.config';
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'build'>;
+  const { forgeConfigSelf } = forgeEnv;
+  const define = getBuildDefine(forgeEnv);
+  const config: UserConfig = {
+    build: {
+      lib: {
+        entry: forgeConfigSelf.entry!,
+        fileName: () => '[name].js',
+        formats: ['cjs'],
+      },
+      rollupOptions: {
+        external,
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    },
+    plugins: [pluginHotRestart('restart')],
+    define,
+    resolve: {
+      // Load the Node.js entry.
+      mainFields: ['module', 'jsnext:main', 'jsnext'],
+    },
+  };
+
+  return mergeConfig(getBuildConfig(forgeEnv), config);
+});
