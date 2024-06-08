1de84c431652ab653ee8f11458a79c7582e1a8a9
Samuel Hoyas <samuelix97@gmail.com>
2024-06-08 18:23:49 +0200

    Play, pause, stop and metronome buttons

---
+++ src/elements/button-icon/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+
+interface ButtonIconProps {
+  icon: string;
+  onClick: () => void;
+  className?: string;
+}
+
+const ButtonIcon = ({ icon, onClick, className = '' }: ButtonIconProps) => {
+  return (
+    <button onClick={onClick} className={`dm-button ${className}`}>
+      <img src={icon} />
+    </button>
+  );
+};
+
+export default ButtonIcon;
