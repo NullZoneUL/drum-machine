08648e554d8af745407a2d39fb31ff90baef1552
Samuel Hoyas <samuelix97@gmail.com>
2024-06-14 00:56:32 +0200

    Instruments context

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,18 +1,47 @@
-import React, { useEffect } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import MainControlsContainer from '@components/main-controls';
 import InstrumentControlsContainer from '@components/instrument-controls';
 import './style.scss';
 
+export const InstrumentsContext = createContext<{
+  instruments: File[];
+  addInstrument: (instrument: File) => void;
+}>({
+  instruments: [],
+  addInstrument: () => {
+    /**/
+  },
+});
+
 const App = () => {
+  const [instruments, setInstrument] = useState<File[]>([]);
+  const instrumentsRef = useRef(instruments);
+
+  useEffect(() => {
+    instrumentsRef.current = instruments;
+  }, [instruments]);
+
   useEffect(() => {
     console.log('Loaded!!');
   }, []);
 
+  const addInstrument = useCallback((instrument: File) => {
+    setInstrument([...instrumentsRef.current, instrument]);
+  }, []);
+
   return (
-    <div className="drum-machine-container">
-      <MainControlsContainer />
-      <InstrumentControlsContainer />
-    </div>
+    <InstrumentsContext.Provider value={{ instruments, addInstrument }}>
+      <div className="drum-machine-container">
+        <MainControlsContainer />
+        <InstrumentControlsContainer />
+      </div>
+    </InstrumentsContext.Provider>
   );
 };
 
--- src/components/instrument-controls/instrument-selector/index.tsx
+++ src/components/instrument-controls/instrument-selector/index.tsx
@@ -1,12 +1,19 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import InputFile from '@elements/input-file';
 import Translation from '@assets/literals/literals';
+import { InstrumentsContext } from '@components/app';
 import './style.scss';
 
 const InstrumentSelectorContainer = () => {
+  const instruments = useContext(InstrumentsContext);
+
   const onFileSelected = useCallback((file: File) => {
-    console.log('TODO!!', file);
+    instruments.addInstrument(file);
   }, []);
+
+  useEffect(() => {
+    console.log(instruments.instruments);
+  }, [instruments]);
 
   return (
     <div className="instrument-selector-container">
