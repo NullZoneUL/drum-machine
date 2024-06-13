9016c8e8115867be851b65d176aaaff7f66654b7
Samuel Hoyas <samuelix97@gmail.com>
2024-06-14 01:31:03 +0200

    Instrument selector

--- src/components/instrument-controls/instrument-selector/index.tsx
+++ src/components/instrument-controls/instrument-selector/index.tsx
@@ -1,8 +1,12 @@
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useCallback, useContext } from 'react';
 import InputFile from '@elements/input-file';
 import Translation from '@assets/literals/literals';
+import DMSelect from '@elements/select';
+import { noItemsList } from '@/utils/default_values';
 import { InstrumentsContext } from '@components/app';
 import './style.scss';
+
+const INSTRUMENT_LIST = 'INSTRUMENT_LIST';
 
 const InstrumentSelectorContainer = () => {
   const instruments = useContext(InstrumentsContext);
@@ -11,16 +15,27 @@ const InstrumentSelectorContainer = () => {
     instruments.addInstrument(file);
   }, []);
 
-  useEffect(() => {
-    console.log(instruments.instruments);
-  }, [instruments]);
-
   return (
     <div className="instrument-selector-container">
       <InputFile
         text={Translation.add_instrument}
         accept="audio/mpeg, audio/wav, audio/ogg"
         callback={onFileSelected}
+      />
+      <DMSelect
+        id={INSTRUMENT_LIST}
+        items={
+          instruments.instruments.length > 0
+            ? instruments.instruments.map(item => {
+                return {
+                  name: item.name,
+                };
+              })
+            : noItemsList
+        }
+        onChange={(index: number) =>
+          console.log(`TODO!!! Selected index: ${index}`)
+        }
       />
     </div>
   );
---
+++ src/utils/default_values.ts
@@ -0,0 +1,1 @@
+export const noItemsList = [{ name: '<none>' }];
