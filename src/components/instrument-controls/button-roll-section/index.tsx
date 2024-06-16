4811607429f271d7a70ae6d6753621513d235ffd
Samuel Hoyas <samuelix97@gmail.com>
2024-06-16 20:40:02 +0200

    Button roll element behavior added

--- src/components/instrument-controls/button-roll-section/index.tsx
+++ src/components/instrument-controls/button-roll-section/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ButtonRoll from '@elements/button-roll';
 import './style.scss';
 
 const NUM_BUTTONS = 16;
 
 const ButtonRollContainer = () => {
+  const onButtonRollClicked = useCallback((index: number) => {
+    console.log(`TODO!!! Button roll click. Index: ${index}`);
+  }, []);
+
   return (
     <div className="buttons-roll-container">
       {[...Array(NUM_BUTTONS)].map((x, i) => (
-        <ButtonRoll number={i + 1} key={`BUTTON_ROLL_${i}`} />
+        <ButtonRoll
+          number={i + 1}
+          key={`BUTTON_ROLL_${i}`}
+          selected={false}
+          playing={false}
+          onClick={onButtonRollClicked}
+        />
       ))}
     </div>
   );
--- src/elements/button-roll/index.tsx
+++ src/elements/button-roll/index.tsx
@@ -3,11 +3,26 @@ import './style.scss';
 
 interface ButtonRollProps {
   number: number;
+  selected: boolean;
+  playing: boolean;
+  onClick: (index: number) => void;
 }
 
-const ButtonRoll = ({ number }: ButtonRollProps) => {
+const ButtonRoll = ({
+  number,
+  selected,
+  playing,
+  onClick,
+}: ButtonRollProps) => {
+  const onClick_ = () => {
+    onClick(number - 1);
+  };
+
   return (
-    <button className="button-roll">
+    <button
+      className={`button-roll ${selected ? 'button-roll-selected' : ''} ${playing ? 'button-roll-playing' : ''}`}
+      onClick={onClick_}
+    >
       <div className="button-roll-container">{number}</div>
     </button>
   );
