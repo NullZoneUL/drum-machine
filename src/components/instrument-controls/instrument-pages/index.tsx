8c7e4daa2868b292c0d4f9e7e11c9a4fba7dd044
Samuel Hoyas <samuelix97@gmail.com>
2024-06-17 19:10:00 +0200

    Light dots and final CSS

--- src/components/instrument-controls/instrument-pages/index.tsx
+++ src/components/instrument-controls/instrument-pages/index.tsx
@@ -1,8 +1,13 @@
 import React, { useCallback, useMemo } from 'react';
 import NumSelector from '@elements/num-selector';
 import ButtonText from '@elements/button-text';
+import LightDotIndicatorContainer from '@elements/light-dot-indicator';
 import Translation from '@assets/literals/literals';
-import { TICKS_BY_PAGE, DEFAULT_MAIN_PAGES } from '@/utils/default_values';
+import {
+  TICKS_BY_PAGE,
+  DEFAULT_MAIN_PAGES,
+  MAX_PAGES,
+} from '@/utils/default_values';
 import { CustomEventNames } from '@/utils/event';
 import { useEventListener } from '@/hooks/event-listener';
 import './style.scss';
@@ -24,18 +29,23 @@ const InstrumentPagesContainer = () => {
 
   return (
     <div className="instrument-pages-container">
-      <div>
-        <NumSelector
-          onChange={(value: number) =>
-            console.log(`TODO!!! Instrument pages value: ${value}`)
-          }
-          minValue={1}
-          maxValue={maxTicksValue}
-          defaultValue={maxTicksValue}
-        />
-        <span>{`/ ${maxTicksValue}`}</span>
+      <div className="instrument-pages-container-center">
+        <div className="instrument-ticks-length">
+          <NumSelector
+            onChange={(value: number) =>
+              console.log(`TODO!!! Instrument pages value: ${value}`)
+            }
+            minValue={1}
+            maxValue={maxTicksValue}
+            defaultValue={maxTicksValue}
+          />
+          <span>{maxTicksValue}</span>
+        </div>
+        <div className="instrument-pages-selector">
+          <LightDotIndicatorContainer numLights={MAX_PAGES} />
+          <ButtonText text={Translation.page} callback={onPageSelect} />
+        </div>
       </div>
-      <ButtonText text={Translation.page} callback={onPageSelect} />
     </div>
   );
 };
--- src/utils/default_values.ts
+++ src/utils/default_values.ts
@@ -3,3 +3,5 @@ export const noItemsList = [{ name: '<none>' }];
 export const TICKS_BY_PAGE = 16;
 
 export const DEFAULT_MAIN_PAGES = 4;
+
+export const MAX_PAGES = 4;
