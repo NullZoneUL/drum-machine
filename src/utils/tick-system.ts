456785ad78307edc18df0c8c0196960d8da632f9
Samuel Hoyas <samuelix97@gmail.com>
2024-06-18 01:37:59 +0200

    Ticks calc

--- src/utils/tick-system.ts
+++ src/utils/tick-system.ts
@@ -2,8 +2,10 @@ import { bpmValues, DEFAULT_MAIN_PAGES, TICKS_BY_PAGE } from './default_values';
 
 let bpm = bpmValues.default;
 let pages = DEFAULT_MAIN_PAGES;
+let tickNumber = 0;
 let timeByTick: number;
 let tickInterval: number;
+let ticksByLoop: number;
 
 export const setNewBPMs = (newValue: number) => {
   if (newValue >= bpmValues.min && newValue <= bpmValues.max) {
@@ -14,13 +16,25 @@ export const setNewBPMs = (newValue: number) => {
 
 export const setNumPages = (num: number) => {
   pages = num;
+  ticksByLoop = getTicksByLoop();
 };
 
+//TODO!!! Add event calls
 export const onPlay = () => {
   const lastTimeByTick = timeByTick;
   onPause();
+
   tickInterval = window.setInterval(() => {
-    console.log('Tick');
+    if (tickNumber % 5 === 0) {
+      if (tickNumber === ticksByLoop) {
+        tickNumber = 0;
+      }
+      console.log('General tick event: ', tickNumber);
+    }
+    console.log('Tick: ', tickNumber);
+
+    tickNumber++;
+
     if (timeByTick !== lastTimeByTick) {
       onPlay();
     }
@@ -33,14 +47,22 @@ export const onPause = () => {
 
 export const onStop = () => {
   onPause();
-  //TODO!! Add stop behavior
+  tickNumber = 0;
+  //TODO!!! Add reset event call
 };
 
 const getTimeByTick = () => {
   const quarterTime = 60000 / bpm;
   const quaverTime = quarterTime / 4;
-  const sectionTime = quaverTime / 4;
+  const sectionTime = quaverTime / 5;
   return sectionTime;
 };
 
+const getTicksByLoop = () => {
+  const totalQuaverTicks = pages * TICKS_BY_PAGE;
+  const totalTicksBySection = totalQuaverTicks * 5;
+  return totalTicksBySection;
+};
+
 timeByTick = getTimeByTick();
+ticksByLoop = getTicksByLoop();
