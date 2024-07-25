0e71a21cf472dac6d0052316968da71b3321e2d5
Samuel Hoyas <samuelix97@gmail.com>
2024-07-25 19:49:11 +0200

    Instrument page selector fix for new instruments

--- src/hooks/ticks-pages.ts
+++ src/hooks/ticks-pages.ts
@@ -3,11 +3,15 @@ import { CustomEventNames } from '@utils/event';
 import { DEFAULT_MAIN_PAGES, TICKS_BY_PAGE } from '@utils/default_values';
 import { useEventListener } from './event-listener';
 
+let acutalMainPages = DEFAULT_MAIN_PAGES;
+
 export const useTicksPagesListener = () => {
   const mainNumPages = useEventListener(
     CustomEventNames.mainPages,
-    DEFAULT_MAIN_PAGES,
+    acutalMainPages,
   );
+
+  acutalMainPages = mainNumPages;
 
   const maxTicksValue = useMemo(
     () => TICKS_BY_PAGE * mainNumPages,
